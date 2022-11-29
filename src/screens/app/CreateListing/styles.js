8431e693ec8a0c07c5e75620914b1298aca10dbf
Anastasios Lalos <tlalos@hotmail.com>
2022-11-29 15:52:11 +0200

    Create new listing Screen

--- src/screens/app/CreateListing/styles.js
+++ src/screens/app/CreateListing/styles.js
@@ -1,34 +1,70 @@
-import {Dimensions, StyleSheet} from 'react-native';
-import {colors} from '../../../utils/colors';
-
-const {height} = Dimensions.get('window');
+import { StyleSheet } from "react-native";
+import { colors } from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-  item: {
-    padding: 8,
-    paddingHorizontal: 16,
-    marginVertical: 8,
-  },
-  sectionTitle: {
-    fontWeight: '500',
-    fontSize: 16,
-    color: colors.grey,
-    marginBottom: 16,
-  },
-  sectionHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  icon: {
-    width: 24,
-    height: 24,
-    
-  },
-  button: {
-    paddingVertical: 16,
-    marginTop: 16,
-  },
-});
+    container: {
+        padding: 24,
+    },
+    sectionTitle: {
+        fontWeight: '500',
+        fontSize: 14,
+        color: colors.blue,
+        marginBottom: 16,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 4,
+    },
+    uploadContainer: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: colors.grey,
+        borderStyle: 'dotted',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8,
+        marginTop: 8,
+    },
+    uploadCircle: {
+        width: 32,
+        height: 32,
+        borderRadius: 20,
+        backgroundColor: colors.lightGrey,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    uploadPlus: {
+        color: colors.white,
+        fontSize: 28,
+        marginTop: -4,
+    },
+    imageRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        paddingBottom: 16,
+    },
+    imageCont: {
+        flexDirection: 'row',
+        marginTop: 8,
+        marginRight: 8,
+    },
+    delete: {
+        width: 24,
+        height: 24,
+        marginLeft: -16,
+        marginTop: -10,
+    },
+    textarea: {
+        minHeight: 150,
+        paddingTop: 16,
+    },
+    button: {
+        marginBottom: 160,
+    }
+})
